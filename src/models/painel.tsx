7038ab6df522cbc05d6e7cfe8bfc20c854094953
antoniobarolo <antoniobarolog@gmail.com>
2023-03-29 16:52:37 -0300

    Updates

--- src/models/painel.tsx
+++
@@ -1,9 +0,0 @@
-export default interface Painel {
-	titulo?: string
-	subtitulo?: string
-	texto?: string,
-	ul?: string[],
-	img?: string,
-	link?: string,
-	video?: string
-};
--- src/models/secao.tsx
+++
@@ -1,6 +0,0 @@
-export default interface Secao {
-	titulo: string;
-	texto?: string;
-	link?: string;
-	bgImg?: string;
-};
